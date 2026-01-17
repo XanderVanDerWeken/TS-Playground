9846b6a5414ae66eada2f54e29fd43e46235268f
Xander VanDerWeken <vanxa812@yahoo.de>
2026-01-17 22:03:14 +0100

    Adding API to publish

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,12 @@
 import amqp from 'amqplib';
+import { randomUUID } from 'crypto';
 import type { UserCreated } from '@app/core';
+import Fastify from 'fastify';
+import { CreateUserSchema } from './schemas/createUser';
+
+const server = Fastify({ logger: true });
 
 async function main() {
-  //const conn = await amqp.connect("amqp://localhost:5672", );
   const conn = await amqp.connect({
     hostname: "localhost",
     port: 5672,
@@ -14,18 +18,35 @@ async function main() {
   const queue = "users";
   await channel.assertQueue(queue);
 
-  const event: UserCreated = {
-    type: "user.created",
-    payload: {
-      user: {
-        id: 1,
-        email: "some@email.com"
-      }
-    }
-  };
+  server.post('/users', async (request, reply) => {
+    const parsed = CreateUserSchema.safeParse(request.body);
 
-  channel.sendToQueue(queue, Buffer.from(JSON.stringify(event)));
-  console.log("User Created sent");
+    if (!parsed.success) {
+      return reply.status(400).send(parsed.error);
+    }
+
+    const user = {
+      id: -1,
+      email: parsed.data.email,
+    };
+
+    const event: UserCreated = {
+      id: randomUUID().toString(),
+      type: "user.created",
+      timestamp: new Date().toISOString(),
+      payload: {
+        user: user,
+      }
+    };
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(event)));
+
+    return reply.status(201).send(user);
+  });
+
+  await server.listen({
+    port: 3000,
+  })
 }
 
 main();
